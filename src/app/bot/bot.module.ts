d6e38f517254be55cc782f21e9d84f0f8097772a
w00s44 <oleg.krolik@gmail.com>
2017-09-24 02:40:50 +0300

    shopping cart test

--- src/app/bot/bot.module.ts
+++ src/app/bot/bot.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BotComponent } from './bot.component';
 import { FormsModule } from '@angular/forms';
 import { BotService } from './service/bot.service';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [BotComponent],
+  declarations: [BotComponent, ShoppingCartComponent],
   providers: [BotService],
   exports: [BotComponent]
 })
