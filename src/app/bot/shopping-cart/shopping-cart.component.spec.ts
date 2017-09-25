c2f51dccca38b6c84839e970169b71242274e079
w00s44 <23149974+w00s44@users.noreply.github.com>
2017-09-25 16:49:41 +0300

    Update shopping-cart.component.spec.ts

--- src/app/bot/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/bot/shopping-cart/shopping-cart.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShoppingCartComponent } from './shopping-cart.component';
+import { FormsModule } from '@angular/forms';
 
 describe('ShoppingCartComponent', () => {
   let component: ShoppingCartComponent;
@@ -8,6 +9,7 @@ describe('ShoppingCartComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [ ShoppingCartComponent ]
     })
     .compileComponents();
